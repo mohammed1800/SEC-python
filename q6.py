129552a490585d67279f609c307c3614b3c68215
mohammed1800 <119121182+mohammed1800@users.noreply.github.com>
2024-04-16 04:49:12 +0530

    Create q6.py

---
+++ q6.py
@@ -0,0 +1,9 @@
+# A sequence is an ordered collection of items. It can be a list, tuple, string, or any other iterable
+# object in Python. For the sake of this example, let's use a list as our sequence.
+
+# Define a list as our sequence
+my_list = [10, 20, 30, 40, 50]
+
+# Loop over the elements of the list using a for loop
+for item in my_list:
+    print(item)
